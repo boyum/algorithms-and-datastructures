0d2b95507ba58ab3890b06b2dfd4cde8262ad2d4
Sindre Bøyum <sindre@acos.no>
2019-04-02 18:54:38 +0200

    Updated dependencies with breaking changes

--- js/lists/linked-list/linked-list.test.js
+++ js/lists/linked-list/linked-list.test.js
@@ -1,7 +1,8 @@
-import test from 'ava';
-// Import {iterate} from 'leakage';
 import LinkedList from './linked-list';
 import LinkedListNode from './linked-list-node';
+import test from 'ava';
+
+// Import {iterate} from 'leakage';
 
 /** @type {LinkedList} */
 let list;
@@ -23,26 +24,26 @@ test.afterEach('Clear list', () => {
   list.clear();
 });
 
-test('size() returns the size of the list', t => {
+test.serial('size() returns the size of the list', t => {
   t.is(list.size(), 4);
   t.not(list.size(), 3);
 });
 
-test('clear() removes every element from the list', t => {
+test.serial('clear() removes every element from the list', t => {
   list.clear();
 
   t.is(list.size(), 0);
   t.is(list._first, null);
 });
 
-test('clear() does not break when used on an empty list', t => {
+test.serial('clear() does not break when used on an empty list', t => {
   const newList = new LinkedList();
   newList.clear();
 
   t.is(newList._first, null);
 });
 
-test('clear() does not break when used on a list with only one element', t => {
+test.serial('clear() does not break when used on a list with only one element', t => {
   const newList = new LinkedList();
   newList.add('item');
   newList.clear();
@@ -50,7 +51,7 @@ test('clear() does not break when used on a list with only one element', t => {
   t.is(newList._first, null);
 });
 
-test('clear() empties the entire list', t => {
+test.serial('clear() empties the entire list', t => {
   let count = 0;
 
   list.clear();
@@ -62,7 +63,7 @@ test('clear() empties the entire list', t => {
   t.is(count, 0);
 });
 
-// Test('clear() doesn\'t leak', () => {
+// test.serial('clear() doesn\'t leak', () => {
 //   const newList = new LinkedList();
 
 //   iterate(() => {
@@ -80,7 +81,7 @@ test('clear() empties the entire list', t => {
 //   });
 // });
 
-test('add() sets the list\'s `_first` item to the first if the list was empty', t => {
+test.serial('add() sets the list\'s `_first` item to the first if the list was empty', t => {
   const newList = new LinkedList();
 
   t.is(newList._first, null);
@@ -90,34 +91,36 @@ test('add() sets the list\'s `_first` item to the first if the list was empty', 
   t.is(newList._first._data, 'item');
 });
 
-test('add() adds an item to the end of the list', t => {
+test.serial('add() adds an item to the end of the list', t => {
   list.add('5');
+
+  console.log('list', list);
 
   t.is(list.size(), 5);
 });
 
-test('add() sets the second last item\'s `_next` value to the new item\'s node', t => {
+test.serial('add() sets the second last item\'s `_next` value to the new item\'s node', t => {
   const secondLast = list.getLastNode();
   list.add('5');
 
   t.is(secondLast._next._data, '5');
 });
 
-test('addAtIndex() adds an item to the specified index', t => {
+test.serial('addAtIndex() adds an item to the specified index', t => {
   const el = '5';
   list.addAtIndex(el, 3);
 
   t.is(list.get(3), el);
 });
 
-test('addAtIndex(item, 0) adds an item to the beginning of the list', t => {
+test.serial('addAtIndex(item, 0) adds an item to the beginning of the list', t => {
   const el = '5';
   list.addAtIndex(el, 0);
   console.log(list.toString());
   t.is(list.get(0), el);
 });
 
-test('addAtIndex() throws an error if index is greater than list size', t => {
+test.serial('addAtIndex() throws an error if index is greater than list size', t => {
   const el = '5';
   const size = list.size();
   const index = size;
@@ -128,7 +131,7 @@ test('addAtIndex() throws an error if index is greater than list size', t => {
   t.is(error.message, `Index (${index}) is greater than or equal to size (${size}).`);
 });
 
-// Test('addAll(list) adds every element of a list to the end of the list', t => {
+// test.serial('addAll(list) adds every element of a list to the end of the list', t => {
 //   const newList = new LinkedList();
 
 //   t.is(list.size(), 4);
@@ -140,14 +143,14 @@ test('addAtIndex() throws an error if index is greater than list size', t => {
 //   t.is(list.size(), 6);
 // });
 
-test('addFirst() adds the specified element to the start of the list', t => {
+test.serial('addFirst() adds the specified element to the start of the list', t => {
   const el = '0';
   list.addFirst(el);
 
   t.is(list.get(0), el);
 });
 
-test('clone() creates a copy of the list', t => {
+test.serial('clone() creates a copy of the list', t => {
   const clone = list.clone();
   clone.add('5');
 
@@ -155,7 +158,7 @@ test('clone() creates a copy of the list', t => {
   t.is(list.size(), 4);
 });
 
-test('contains() returns true if the list contains the specified element', t => {
+test.serial('contains() returns true if the list contains the specified element', t => {
   const el = '5';
   list.add(el);
 
@@ -163,17 +166,17 @@ test('contains() returns true if the list contains the specified element', t => 
   t.false(list.contains('6'));
 });
 
-test('contains() returns false if the list is empty', t => {
+test.serial('contains() returns false if the list is empty', t => {
   const newList = new LinkedList();
 
   t.false(newList.contains('item'));
 });
 
-test('contains() returns false if no parameter', t => {
+test.serial('contains() returns false if no parameter', t => {
   t.false(list.contains());
 });
 
-test('indexOf() returns the first index of the specified element, or -1', t => {
+test.serial('indexOf() returns the first index of the specified element, or -1', t => {
   const el = '5';
   const el2 = '5';
 
@@ -184,17 +187,17 @@ test('indexOf() returns the first index of the specified element, or -1', t => {
   t.is(list.indexOf('6'), -1);
 });
 
-test('indexOf() returns -1 if the list is empty', t => {
+test.serial('indexOf() returns -1 if the list is empty', t => {
   const newList = new LinkedList();
 
   t.is(newList.indexOf('item'), -1);
 });
 
-test('indexOf() returns -1 if no parameter', t => {
+test.serial('indexOf() returns -1 if no parameter', t => {
   t.is(list.indexOf(), -1);
 });
 
-test('lastIndexOf() returns the last index of the specified element, or -1', t => {
+test.serial('lastIndexOf() returns the last index of the specified element, or -1', t => {
   const el = '5';
   const el2 = '5';
 
@@ -204,34 +207,34 @@ test('lastIndexOf() returns the last index of the specified element, or -1', t =
   t.is(list.lastIndexOf('5'), 5);
 });
 
-test('lastIndexOf() returns -1 if the list is empty', t => {
+test.serial('lastIndexOf() returns -1 if the list is empty', t => {
   const newList = new LinkedList();
 
   t.is(newList.lastIndexOf('item'), -1);
 });
 
-test('lastIndexOf() returns -1 if no parameter', t => {
+test.serial('lastIndexOf() returns -1 if no parameter', t => {
   t.is(list.lastIndexOf(), -1);
 });
 
-test('element() retrieves, but does not remove, the first element of the list', t => {
+test.serial('element() retrieves, but does not remove, the first element of the list', t => {
   const el = list.element();
 
   t.is(el, '1');
 });
 
-test('element() returns null if the list is empty', t => {
+test.serial('element() returns null if the list is empty', t => {
   const newList = new LinkedList();
 
   t.is(newList.element(), null);
 });
 
-test('get() returns the element at the specified position', t => {
+test.serial('get() returns the element at the specified position', t => {
   t.is(list.get(0), '1');
   t.is(list.get(list.size() - 1), '4');
 });
 
-test('get() throws a RangeError if the specified index is greater than or equal to the list\'s size', t => {
+test.serial('get() throws a RangeError if the specified index is greater than or equal to the list\'s size', t => {
   const size = list.size();
   const index = size;
   const error = t.throws(() => {
@@ -241,12 +244,12 @@ test('get() throws a RangeError if the specified index is greater than or equal 
   t.is(error.message, `Index (${index}) is greater than or equal to size (${size}).`);
 });
 
-test('getNode() returns the node at the specified position', t => {
+test.serial('getNode() returns the node at the specified position', t => {
   t.is(list.getNode(0)._data, '1');
   t.is(list.getNode(list.size() - 1)._data, '4');
 });
 
-test('getNode() throws a RangeError if the specified index is greater than or equal to the list\'s size', t => {
+test.serial('getNode() throws a RangeError if the specified index is greater than or equal to the list\'s size', t => {
   const size = list.size();
   const index = size;
   const error = t.throws(() => {
@@ -256,27 +259,27 @@ test('getNode() throws a RangeError if the specified index is greater than or eq
   t.is(error.message, `Index (${index}) is greater than or equal to size (${size}).`);
 });
 
-test('getLast() returns the last element', t => {
+test.serial('getLast() returns the last element', t => {
   t.is(list.getLast(), '4');
 });
 
-test('getLast() returns null if the list is empty', t => {
+test.serial('getLast() returns null if the list is empty', t => {
   const newList = new LinkedList();
 
   t.is(newList.getLast(), null);
 });
 
-test('getLastNode() returns the last node', t => {
+test.serial('getLastNode() returns the last node', t => {
   t.is(list.getLastNode()._data, '4');
 });
 
-test('getLastNode() returns null if the list is empty', t => {
+test.serial('getLastNode() returns null if the list is empty', t => {
   const newList = new LinkedList();
 
   t.is(newList.getLastNode(), null);
 });
 
-test('toNodeArray() returns the list\'s nodes as an array', t => {
+test.serial('toNodeArray() returns the list\'s nodes as an array', t => {
   const newList = new LinkedList();
 
   const el1 = 'item1';
@@ -294,7 +297,7 @@ test('toNodeArray() returns the list\'s nodes as an array', t => {
   t.is(newList.toNodeArray()[1]._next, null);
 });
 
-test('toArray() returns the list\'s data as an array', t => {
+test.serial('toArray() returns the list\'s data as an array', t => {
   const newList = new LinkedList();
 
   const el1 = 'item1';
@@ -309,7 +312,7 @@ test('toArray() returns the list\'s data as an array', t => {
   t.is(newList.toArray()[1], arr[1]);
 });
 
-test('toString() returns the list\'s data as a string', t => {
+test.serial('toString() returns the list\'s data as a string', t => {
   const newList = new LinkedList();
 
   const el1 = 'item1';
